a2afdc0bd88f798418e6b1fa38105389293b3b3c
rohit-shotang <rohit@shotang.com>
2017-04-03 02:18:43 +0530

    new build

--- src/components/User.vue
+++ src/components/User.vue
@@ -64,6 +64,24 @@
 import db from '../db/FirebaseDb'
 import toastr from 'toastr'
 
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+
 let usersRef = db.ref('users')
 
 export default {
